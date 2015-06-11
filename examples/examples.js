0aa24292241b83bf06e31266c9ba9071ec8a3cd8
Robert Pitt <rpitt@centiq.co.uk>
2015-06-11 19:53:42 +0100

    WIP, restructure and bug fixing

--- examples/examples.js
+++ examples/examples.js
@@ -6,14 +6,19 @@ var BinLog  = require("../");
 var binLog = new BinLog({
 	"host": "127.0.0.1",
 	"user": "zongji",
-	"password": "zongji"
+	"password": "zongji",
+	// debug: true
 });
 
 /**
  * Use specific event parsers
  */
-binLog.use(BinLog.Parsers.Rotate);
-
-binLog.on("rotate", f)
+binLog.use(BinLog.Packets.Rotate);
+binLog.use(BinLog.Packets.Format);
+binLog.use(BinLog.Packets.Query);
+// binLog.use(BinLog.Packets.Xid);
+// binLog.use(BinLog.Packets.Stop);
+// binLog.use(BinLog.Packets.TableMap);
+// binLog.use(BinLog.Packets.UpdateRows);
 
 binLog.start();
